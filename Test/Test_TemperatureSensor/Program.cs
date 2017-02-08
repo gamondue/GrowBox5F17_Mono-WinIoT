9f1a4e8a9a6b3cc78948829e9e540c46b391cc59
unknown <andrea.pantieri@WS3817.local.ittcesena.it>
2017-02-08 11:44:06 +0100

    Sistemati i namespace e le referenze

--- Test/Test_TemperatureSensor/Program.cs
+++ Test/Test_TemperatureSensor/Program.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GrowBoxShared;
 
-namespace GrowBoxShared
+//Test_TemperatureSensor
+namespace Test_TemperatureSensor
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
         }
     }
 }
