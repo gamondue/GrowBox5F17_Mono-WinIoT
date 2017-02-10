b2bd51cce266f943c2d8f5c1fe557fcadcd8386d
gamondue <gamondue@gmail.com>
2017-02-10 13:59:28 +0100

    Aggiunti programmi di test non ancora assegnati ai gruppi

---
+++ Test/Test_FileLogger/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_FileLogger
+{
+    class Program
+    {
+        //TODO lavoro da assegbare !!!!
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Test/Test_OutShifRegister/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GrowBoxShared;
+
+/// <summary>
+/// Programma di prova per lo shift register di uscita per le attuazioni GrowBox
+/// </summary>
+namespace Test_OutShifRegister
+{
+    class Program
+    {
+        //TODO assegnare il lavoro ad un gruppo !!!!!
+        static void Main(string[] args)
+        {
+            OutShiftRegister sr = new GrowBoxShared.OutShiftRegister(16, 25, 24, 23);
+        }
+    }
+}
