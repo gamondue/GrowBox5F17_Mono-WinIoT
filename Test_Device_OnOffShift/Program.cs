11e37fd8b8f54c865debb59de04d2eee6695f200
GiackAloZ <giacomo.aloisi@local.ittcesena.it>
2017-02-11 10:27:08 +0100

    Added test program

--- Test_Device_OnOffShift/Program.cs
+++ Test_Device_OnOffShift/Program.cs
@@ -12,25 +12,50 @@ namespace Test_Device_OnOffShift
     {
         static void Main(string[] args)
         {
-            const int NUM_BITS = 16;
-            const int DATA_PIN = 16;
-            const int CLOCK_PIN = 16;
-            const int LATCH_PIN = 16;
+            Device_OnOffShift a = new Device_OnOffShift(new OutShiftRegister(16, 25, 24, 23), 2/* num bit*/);
+            a.WriteLog += A_WriteLog;
+            for (bool running = true; running;)
+            {
 
-            const int ONOFFSHIFT_BIT = 16;
+                Console.WriteLine("Selezionare l'opzione desiderata tramite numero:");
+                Console.WriteLine("/t1- On");
+                Console.WriteLine("/t2.-Off");
+                Console.WriteLine("/t3-On temporizzato");
+                Console.WriteLine("/t4-Spegni e chiudi");
+                string s = Console.ReadLine();
+                if (s == "3")
+                {
+                    int time = 0;
+                    bool ok;
+                    do
+                    {
+                        Console.WriteLine("Inserire il tempo(ms)... ");
+                        ok = int.TryParse(Console.ReadLine(), out time);
+                        if (!ok)
+                        {
+                            Console.WriteLine("Errore nella stringa di input");
+                        }
+                    } while (!ok);
+                    a.TimeOn(time);
+                }
+                else if (s == "2")
+                {
+                    a.Off();
+                }
+                else if (s == "1")
+                    a.On();
+                else if (s == "4")
+                {
+                    a.Off();
+                    running = false;
+                }
+            }
+        }
 
-            OutShiftRegister reg = new OutShiftRegister(NUM_BITS, DATA_PIN, CLOCK_PIN, LATCH_PIN);
-            Device_OnOffShift tester = new Device_OnOffShift(reg, ONOFFSHIFT_BIT);
-            tester.On();
-            Console.WriteLine("Tester on");
-            Task.Delay(1000);
-            tester.Off();
-            Console.WriteLine("Tester off");
-            Task.Delay(500);
-            Console.WriteLine("Tester on for 2 seconds");
-            tester.TimeOn(2000);
-            Task.Delay(2000);
-            Console.WriteLine("Test termined");
+        private static void A_WriteLog(string line)
+        {
+            FileLogger fl = new FileLogger("TestDevice On Off");
+            fl.WriteLogger(line);
         }
     }
 }
