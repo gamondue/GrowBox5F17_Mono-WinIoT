e0abb5d4a567353e505ae6358c5218cb037332af
GiackAloZ <giacomo.aloisi@local.ittcesena.it>
2017-02-11 10:42:48 +0100

    Added led test pins

--- Test_Device_OnOffShift/Program.cs
+++ Test_Device_OnOffShift/Program.cs
@@ -14,14 +14,24 @@ namespace Test_Device_OnOffShift
         {
             Device_OnOffShift a = new Device_OnOffShift(new OutShiftRegister(16, 25, 24, 23), 2/* num bit*/);
             a.WriteLog += A_WriteLog;
+
+            const int chanLed1 = 40;
+            const int chanLed2 = 38;
+            const int chanButton1 = 37;
+            const int chanButton2 = 35;
+            DigitalIO pinIn1 = new DigitalIO(chanButton1, GpioPinDriveMode.Input);
+            DigitalIO pinIn2 = new DigitalIO(chanButton2, GpioPinDriveMode.Input);
+            DigitalIO pinOut1 = new DigitalIO(chanLed1, GpioPinDriveMode.Output);
+            DigitalIO pinOut2 = new DigitalIO(chanLed2, GpioPinDriveMode.Output);
+
             for (bool running = true; running;)
             {
 
                 Console.WriteLine("Selezionare l'opzione desiderata tramite numero:");
-                Console.WriteLine("/t1- On");
-                Console.WriteLine("/t2.-Off");
-                Console.WriteLine("/t3-On temporizzato");
-                Console.WriteLine("/t4-Spegni e chiudi");
+                Console.WriteLine("\t1- On");
+                Console.WriteLine("\t2.-Off");
+                Console.WriteLine("\t3-On temporizzato");
+                Console.WriteLine("\t4-Spegni e chiudi");
                 string s = Console.ReadLine();
                 if (s == "3")
                 {
@@ -37,16 +47,24 @@ namespace Test_Device_OnOffShift
                         }
                     } while (!ok);
                     a.TimeOn(time);
+                    pinOut1.Write(GpioPinValue.High);
+                    Task.Delay(time);
+                    pinOut1.Write(GpioPinValue.Low);
                 }
                 else if (s == "2")
                 {
                     a.Off();
+                    pinOut1.Write(GpioPinValue.Low);
                 }
                 else if (s == "1")
+                {
                     a.On();
+                    pinOut1.Write(GpioPinValue.High);
+                }
                 else if (s == "4")
                 {
                     a.Off();
+                    pinOut1.Write(GpioPinValue.Low);
                     running = false;
                 }
             }
@@ -54,7 +72,7 @@ namespace Test_Device_OnOffShift
 
         private static void A_WriteLog(string line)
         {
-            FileLogger fl = new FileLogger("TestDevice On Off");
+            FileLogger fl = new FileLogger("test_deviceOnOffShift.txt");
             fl.WriteLogger(line);
         }
     }
